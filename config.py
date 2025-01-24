664d18d4369746bd79829ed72922aac0f0113af9
OGNOBITA12 <sumit07638@gmail.com>
2025-01-24 23:14:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -94,16 +94,16 @@ START_IMG_URL = getenv(
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://files.catbox.moe/4675vq.jpg"
 )
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/14eb59ea7d31229d8d751.jpg"
-STATS_IMG_URL = "https://te.legra.ph/file/ec19cf227791a167abedc.jpg"
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/923c1faac33d8c70335dc.jpg"
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6c66f8b192532fe758e82.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/ebc4dc6357be06e08a3ed.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/d339f390ec168c19879c6.jpg"
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/ee0cd53ab73f08f4a3627.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/5f9fb5bba66021c782d96.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/affe0afec5c7ad63676a4.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/3c446e8dee78ed0ca62ff.jpg"
+PLAYLIST_IMG_URL = "https://carder.ton.run/imagens/1737740456295-780753388.jpg"
+STATS_IMG_URL = "https://carder.ton.run/imagens/1737740456295-780753388.jpg"
+TELEGRAM_AUDIO_URL = "https://carder.ton.run/imagens/1737740456295-780753388.jpg"
+TELEGRAM_VIDEO_URL = "https://carder.ton.run/imagens/1737740456295-780753388.jpg"
+STREAM_IMG_URL = "https://carder.ton.run/imagens/1737740456295-780753388.jpg"
+SOUNCLOUD_IMG_URL = "https://carder.ton.run/imagens/1737740456295-780753388.jpg"
+YOUTUBE_IMG_URL = "https://carder.ton.run/imagens/1737740456295-780753388.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://carder.ton.run/imagens/1737740456295-780753388.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://carder.ton.run/imagens/1737740456295-780753388.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://carder.ton.run/imagens/1737740456295-780753388.jpg"
 
 
 def time_to_seconds(time):
